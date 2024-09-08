3c49d09535dfed5a4456bce0f087aba80c52b258
knknkn1162 <knknkn1162@gmail.com>
2024-09-08 21:07:27 +0900

    fix subnets

--- mssql/subnets.tf
+++ mssql/subnets.tf
@@ -5,4 +5,7 @@ module "subnets" {
   label2cidr = {
     "db" = var.db_cidr
   }
+  # Subnets of virtual network do have ServiceEndpoints for Microsoft.Sql resources configured.
+  # Add Microsoft.Sql to subnet's ServiceEndpoints collection before trying to ACL Microsoft.Sql resources to these subnets
+  service_endpoints = ["Microsoft.Sql"]
 }
