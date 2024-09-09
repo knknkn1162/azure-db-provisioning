a1734d64bbf03e17ae1b2e270f777c2f96621d10
knknkn1162 <knknkn1162@gmail.com>
2024-09-09 19:17:04 +0900

    psql_command

--- db/PostgreSQL/output.tf
+++ db/PostgreSQL/output.tf
@@ -1,3 +1,7 @@
 output "fqdn" {
   value = azurerm_postgresql_flexible_server.example.fqdn
 }
+
+output "psql_command" {
+  value = "PGPASSWORD=${var.db_password} psql -h ${azurerm_postgresql_flexible_server.example.fqdn} -U ${local.admin_name} -p ${local.db_port} -d ${var.db_name}"
+}
