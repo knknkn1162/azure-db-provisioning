ed6596d422529639ec87924415a6d827e710bcb6
knknkn1162 <knknkn1162@gmail.com>
2024-09-09 18:55:07 +0900

    flexible server

--- db/PostgreSQL/db.tf
+++ db/PostgreSQL/db.tf
@@ -10,40 +10,30 @@ locals {
   db_prefix = module.random.result
 }
 
-resource "azurerm_postgresql_server" "example" {
-  name                = local.db_prefix
-  location            = module.resource_group.location
-  resource_group_name = module.resource_group.name
-
-  administrator_login          = local.admin_name
-  administrator_login_password = var.db_password
-
-  sku_name   = var.db_spec
-  version    = var.db_version
+resource "azurerm_postgresql_flexible_server" "example" {
+  # should be unique
+  name                   = local.db_prefix
+  resource_group_name    = module.resource_group.name
+  location               = module.resource_group.location
+  version                = var.db_version
+  # by default
+  # public_network_access_enabled = true
+  administrator_login    = "adminuser"
+  administrator_password = var.db_password
+  # If the storage_mb field is undefined on the initial deployment of the PostgreSQL Flexible Server resource it will default to 32768(minimum).
   storage_mb = var.db_size_mb
-  public_network_access_enabled    = true
-  ssl_enforcement_enabled          = false
-  # ssl_minimal_tls_version_enforced must be set to TLSEnforcementDisabled when ssl_enforcement_enabled is set to false.
-  ssl_minimal_tls_version_enforced = "TLSEnforcementDisabled"
+  sku_name = var.db_spec
 }
 
-resource "azurerm_postgresql_database" "example" {
-  name                = var.db_name
-  resource_group_name = module.resource_group.name
-  server_name         = azurerm_postgresql_server.example.name
-  charset             = "UTF8"
-  collation           = "English_United States.1252"
-
-  # prevent the possibility of accidental data loss
-  # lifecycle {
-  #   prevent_destroy = true
-  # }
+resource "azurerm_postgresql_flexible_server_database" "example" {
+  name      = var.db_name
+  server_id = azurerm_postgresql_flexible_server.example.id
 }
 
-resource "azurerm_postgresql_firewall_rule" "example" {
+
+resource "azurerm_postgresql_flexible_server_firewall_rule" "example" {
   name                = "fw-${uuid()}"
-  resource_group_name = module.resource_group.name
-  server_name         = azurerm_postgresql_server.example.name
+  server_id         = azurerm_postgresql_flexible_server.example.id
   start_ip_address    = "0.0.0.1"
   end_ip_address      = "255.255.255.254"
 }
