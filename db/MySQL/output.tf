4073a4e132f791513144b913f01a11d5e951fd8c
knknkn1162 <knknkn1162@gmail.com>
2024-09-09 22:31:04 +0900

    improve output

--- db/MySQL/output.tf
+++ db/MySQL/output.tf
@@ -14,6 +14,6 @@ output "sshpass" {
   value = "sshpass -p${var.db_password} ssh ${local.admin_name}@${local.db_public_ip}"
 }
 
-output "psql_command" {
-  value = "PGPASSWORD=${var.db_password} psql -h ${local.db_public_ip} -p ${local.db_port} -U ${local.admin_name} ${var.db_name}"
+output "mysql_command" {
+  value = "mysql --host=${local.db_public_ip} --user=${local.admin_name} --password=${var.db_password} --database=${var.db_name} --port=${local.db_port}"
 }
